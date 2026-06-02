1bcd8af3694f547214826fc573596f6e30e80a0b
devisingh2007 <devisinghrajput.cg@gmail.com>
2026-06-02 16:50:00 +0530

    Update src/routes/preferenceRoutes.js

--- backend/src/routes/preferenceRoutes.js
+++ backend/src/routes/preferenceRoutes.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const preferenceController = require('../controllers/preferenceController');
+const { protect } = require('../middlewares/auth');
+const { validate } = require('../middlewares/validation');
+const { preferencesSchema } = require('../validators/preferencesValidator');
+
+router.use(protect);
 
 router.get('/', preferenceController.getUserPreferences);
-router.post('/', preferenceController.updateUserPreferences);
+router.post('/', validate(preferencesSchema), preferenceController.updateUserPreferences);
 
 module.exports = router;
