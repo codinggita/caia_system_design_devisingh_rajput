d43fdc4cb582127c51166791b18d0307d1b8d59c
devisingh2007 <devisinghrajput.cg@gmail.com>
2026-06-02 11:30:00 +0530

    Update src/routes/bookmarkNoteRoutes.js

--- backend/src/routes/bookmarkNoteRoutes.js
+++ backend/src/routes/bookmarkNoteRoutes.js
@@ -2,12 +2,12 @@ const express = require('express');
 const bookmarkNoteController = require('../controllers/bookmarkNoteController');
 const { protect } = require('../middlewares/auth');
 const { validate } = require('../middlewares/validation');
-const { notesListQuerySchema, objectIdSchema } = require('../validators/commonValidator');
+const { notesListQuerySchema, objectIdSchema, paginationSchema } = require('../validators/commonValidator');
 const { addNoteSchema } = require('../validators/conceptValidator');
 
 const router = express.Router();
 
-router.get('/bookmarks', protect, bookmarkNoteController.getMyBookmarks);
+router.get('/bookmarks', protect, validate(paginationSchema, 'query'), bookmarkNoteController.getMyBookmarks);
 router.get('/notes', protect, validate(notesListQuerySchema, 'query'), bookmarkNoteController.getMyNotes);
 router.get('/activity', protect, bookmarkNoteController.getMyActivitySummary);
 router.post('/bookmarks/:id', protect, validate(objectIdSchema, 'params'), bookmarkNoteController.addBookmark);
